552a547a0bf5b3610904470437b90c6f4b0c5dcf
Manoel Malafaia <mmalafaia@gmail.com>
2020-01-03 17:55:25 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {StyleSheet, Animated, View} from 'react-native';
+
+export default class App extends Component {
+  state = {
+    ballY: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    Animated.timing(this.state.ballY, {
+      toValue: 500,
+      duration: 1000,
+    }).start();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.ball, {top: this.state.ballY}]} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+  },
+  ball: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    backgroundColor: '#f00',
+  },
+});
